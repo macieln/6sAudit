d2f223d772a013e8614054eb66416fb3ec2761a4
Nahum Maciel <nahumamaciel@gmail.com>
2024-07-12 15:59:42 -0700

    Began to implement action add button.

--- main.js
+++ main.js
@@ -86,42 +86,48 @@ auditResults.push(
     {
         "step": "Sort",
         "description": "Unnecessary tools and materials are identified and sorted out. This makes the workplace more organized and creates more space for needed work materials.",
-        "result": null
+        "result": null,
+        "actionItems": []
     }
 );
 auditResults.push(
     {
         "step": "Set in order",
         "description": "A system is introduced for arranging the required work materials. Required tools and materials are arranged so that they can be accessed quickly and easily. The frequency of use as well as the order of use of the materials is taken into account.",
-        "result": null
+        "result": null,
+        "actionItems": []
     }
 );
 auditResults.push(
     {
         "step": "Shine",
         "description": "The own workplace and work utensils are cleaned. The cleaning of the work equipment also serves to identify deficiencies as well as potential threads. Thus, a safe and hygienic working environment is created.",
-        "result": null
+        "result": null,
+        "actionItems": []
     }
 );
 auditResults.push(
     {
         "step": "Standardize",
         "description": "All workplaces are standardized. For example, uniform markings and labels are used for work materials. The aim is to create standards that create an optimal working environment and make it easier to change workplaces.",
-        "result": null
+        "result": null,
+        "actionItems": []
     }
 );
 auditResults.push(
     {
         "step": "Sustain",
         "description": "Compliance with the above rules is to become a habit. Likewise, the rules are to be constantly reviewed and improved in order to achieve the best possible benefits.",
-        "result": null
+        "result": null,
+        "actionItems": []
     }
 );
 auditResults.push(
     {
         "step": "Safety",
         "description": "To ensure safety in the workplace, potential sources of danger are searched for. After identifying the sources of danger, potential measures are defined and implemented to minimize the danger and increase safety.",
-        "result": null
+        "result": null,
+        "actionItems": []
     }
 );
 
@@ -143,6 +149,25 @@ function makeAuditStep(index) {
     mainSectionHeader.appendChild(stepInformationButton);
 
     mainSectionContent.appendChild(stepDescription);
+}
+
+function appendActionItem() {
+    (auditResults[index]["actionItems"]).push("CHEESE");
+
+    while (actionItems.lastChild) {
+        actionItems.lastChild.remove();
+    }
+
+    actionItems.appendChild(actionItemsHeader);
+
+    let actionItemDescriptions = auditResults[index]["actionItems"];
+    for (let index in actionItemDescriptions) {
+        let actionItemEL = document.createElement("input");
+        actionItemEL.type = "text";
+        actionItemEL.value = actionItemDescriptions[index];
+
+        actionItems.appendChild(actionItemEL);
+    }
 }
 
 function toggleResult(input) {
@@ -245,6 +270,10 @@ function toggleIndicator(index) {
 
 stepInformationButton.addEventListener("click", () => {
     toggleStepInformation();
+})
+
+addActionButton.addEventListener("click", () => {
+    appendActionItem();
 })
 
 passValidationButton.addEventListener("click", () => {
